f5cfca5ff6f1395da4edc890c0860024a3438c3a
Lucas <lucas.mostyn@hexagon.com>
2025-01-22 19:42:26 -0400

    fix

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,19 @@
-
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask! This will become the spatial job board'
 
+@app.route("/")
+def home():
+    return jsonify({"message": "Hello, World!"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello from Flask! This will become the spatial job board"
